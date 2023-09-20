c4bdd0c8bdd2fe782aa6164d0a0a5ee2b9464e3a
Hannah <Hannah@ICHI>
2023-09-20 16:33:23 +0800

    Add project files.

---
+++ AwesomeMAUIApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeMAUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AwesomeMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace AwesomeMAUIApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string newText = e.NewTextValue;
+            DisplayLbl.Text = newText;
+        }
+
+    }
+}
